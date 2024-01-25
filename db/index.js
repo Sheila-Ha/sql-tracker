946cf8dd0c4f1fb485dd7d0b833bc52e04b79dfa
Sheila Hanson <slhanson11@live.com>
2024-01-25 15:02:35 -0600

    added seeds syntax, add index,js syntax

--- db/index.js
+++ db/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-// Import and require mysql2
+
+//dependencies//
+const express = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -17,21 +18,21 @@ const db = mysql.createConnection(
     user: 'root',
     // MySQL password
     password: '',
-    database: 'classlist_db'
+    database: 'employee_tracker_db'
   },
   console.log(`Connected to the classlist_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM students', function (err, results) {
-  console.log(results);
-});
+// // Query database
+// db.query('SELECT * FROM students', function (err, results) {
+//   console.log(results);
+// });
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,33 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE  employee_trackerDB;
 
-USE employee_trackerDB;
+-- create database --
+DROP DATABASE IF EXISTS employees_DB;
 
+CREATE DATABASE  employees_DB;
+
+USE employees_DB;
+
+-- department table --
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) NOT NULL
 );
 
+-- role table --
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR (30),
-  salary DECIMAL,
+  title VARCHAR (30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+-- employee table --
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR (60),
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR (60) NOT NULL,
   role id INT,
-  manager_id INT NULL
+  manager_id INT
   FOREIGN KEY (role_id) REFERENCES role (id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
