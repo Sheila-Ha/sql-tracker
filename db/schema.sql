d403dd4b8344867521c9969e04ae2753b6d5b369
Sheila Hanson <slhanson11@live.com>
2024-01-25 10:37:21 -0600

    added seeds and updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,27 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE  employee_db;
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE  employee_trackerDB;
 
-USE employee_db;
+USE employee_trackerDB;
 
-CREATE TABLE department(
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
-CREATE TABLE role(
-  id INT NOT NULL,
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR (30),
   salary DECIMAL,
-  department)id INT,
-  PRIMARY KEY (id)
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee(
-  id INT NOT NULL,
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR (60),
   role id INT,
   manager_id INT NULL
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO employee (id, first_name, last_name, role id, manager_id)
+VALUES   (001, "Bob", "Miller"),
+         (002, "Ann", "Atwood"),
+         (003, "Sue", "Woods"),
+         (004, "Tom", "Anderson"),
+         (005, "Will", "Thomas"),
+         (006, "Amy", "Waters");
+
+INSERT INTO role (id, title, salary, department)
+VALUES  (001, 001, "data entry, 2000, development"),
+        (002, 002, "sales, 3000, sales"),
+        (003, 003, "lead engineer, 150000, engineering"),
+        (004, 004, "lawyer, 200000, legal"),
+        (005, 005, "accountant, 175000, finance"),
+        (006, 006, "account manager, 160000, finance");
+
+INSERT INTO department (id, name)
+VALUES  (001, )
