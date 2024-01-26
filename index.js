290f46e86c5f2823ab43a061c73dadb2f406d8a5
Sheila Hanson <slhanson11@live.com>
2024-01-26 15:24:55 -0600

    rearranged script and got basic text working

---
+++ index.js
@@ -0,0 +1,108 @@
+//dependencies//
+const inquirer = require("inquirer");
+const server = require("./utils/server");
+//const mysql = require('mysql2');
+
+//const PORT = process.env.PORT || 3001;
+//const app = express();
+
+//user prompted for choices
+const questions = [
+  {
+    type: "rawlist",
+    name: "choices",
+    message: "Select from options below",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Remove a department",
+      "Remove a role",
+      "Remove an employee",
+    ],
+  },
+];
+
+function processResponse(response) {
+  if (response === "View all departments") {
+    server.getAllDepartments();
+  }
+  if (response === "View all roles") {
+    server.getAllRoles();
+  }
+  if (response === "View all employees") {
+    server.getAllEmployees();
+  }
+  if (response === "Add a department") {
+    addADepartment();
+  }
+  if (response === "Add a role") {
+    addARole();
+  }
+  if (response === "Add an employee") {
+    addAnEmployee();
+  }
+  if (response === "Update an employee role") {
+    updateAEmployeeRole();
+  }
+  if (response === "Remove a department") {
+    removeADepartment();
+  }
+  if (response === "Remove a role") {
+    removeARole();
+  }
+  if (response === "Remove an employee") {
+    removeAnEmployee();
+  }
+}
+
+function init() {
+  //ask questions, then proceed with response
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      //console.log(response.choices);
+      processResponse(response.choices);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// To run: node index.js
+init();
+
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+// Connect to database
+/*const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);*/
+
+// // Query database
+// db.query('SELECT * FROM students', function (err, results) {
+//   console.log(results);
+// });
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
--- utils/server.js
+++ utils/server.js
@@ -1,127 +1,74 @@
 // Include packages needed for this application
 const inquirer = require("inquirer"); //npm special software - const variable can not be reassigned
 const mysql = require("mysql2");
-const fs = require("fs"); //const file system (fs) to read files on my pc
-const generateEmployee = require("./utils/generateEmployee"); //require is built in function, passing location name as argument
+//const fs = require("fs"); //const file system (fs) to read files on my pc
+let roleChoices = [];
 
-//sql connection 
+//sql connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "employee_db",
+  database: "employees_db",
 });
 
 //connect to server and launch app
-db.connect ((err) =>{
+/*db.connect((err) => {
   if (err) throw err;
-  console.log("Hello, welcome to the Employee Tracker.")
-  start();
-});
+  console.log("Hello, welcome to the Employee Tracker.");
+  //start();
+});*/
 
-//user prompted for choices
-const promptUser = () => {
-  inquirer
-    .prompt([
-      {
-        name: "choices",
-        type: "list",
-        message: "Select from options below",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
-          "Remove a department",
-          "Remove a role",
-          "Remove an employee",
-        ],
-      },
-    ])
-    // based on chosen prompt choices then view/add or update
-    .then((answers) => {
-      const { choices } = answers;
-      if (choices === "View all departments") {
-        viewAllDepartments();
-      }
-      if (choices === "View all roles") {
-        viewAllRoles();
-      }
-      if (choices === "View all employees") {
-        viewAllEmployees();
-      }
-      if (choices === "Add a department") {
-        addADepartment();
-      }
-      if (choices === "Add a role") {
-        addARole();
-      }
-      if (choices === "Add an employee") {
-        addAnEmployee();
-      }
-      if (choices === "Update an employee role") {
-        updateAEmployeeRole();
-      }
-      if (choices === "Remove a department") {
-        removeADepartment();
-      }
-      if (choices === "Remove a role") {
-        removeARole();
-      }
-      if (choices === "Remove an employee") {
-        removeAnEmployee();
-      }
-    });
-};
+// db.query("SELECT * FROM role", function (err, results) {
+//   console.log(results);
+//   roleChoices = results;
+// });
 
-//
-const viewAllDepartments 
-
-
-
-
-
-
- //add employee function
-function addEmployee (){
-  const query = 
-
+function getAllDepartments() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.log(results);
+    return results;
+  });
 }
 
-inquirer.prompt([
-  {
-    type:"input",
-    name:"firstName",
-    message:"What is the employee's first name?"
-  },
-  {
-    type:"input",
-    name:"lastName",
-    message:"What is the employee's last name?"
-  },
-  {
-    type:"list",
-    name:"roles",
-    message:"What is the employee's role?",
-    choices: addARole,
-  },
-  {
-    type:"confirm",
-    name:"supervisor",
-    message:"Does the employee have a manager?"
-  },
-  {
-    type:"list",
-    name:"manager",
-    message:"Who is the employee's manager?",
-    choices:name,
-    when:(answers) => answers.supervisor
-  }
-])
+//
+//const viewAllDepartments
 
+//add employee function
+function addEmployee() {
+  const query = "";
+
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "What is the employee's first name?",
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "What is the employee's last name?",
+    },
+    {
+      type: "list",
+      name: "roles",
+      message: "What is the employee's role?",
+      choices: roleChoices,
+    },
+    {
+      type: "confirm",
+      name: "supervisor",
+      message: "Does the employee have a manager?",
+    } /*,
+    {
+      type: "list",
+      name: "manager",
+      message: "Who is the employee's manager?",
+      choices: name,
+      when: (answers) => answers.supervisor,
+    },*/,
+  ]);
+}
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) { // function
@@ -142,3 +89,7 @@ inquirer.prompt([
 // //function call to initialize
 // init();
 //console.log('initializing the app...');
+
+module.exports = {
+  getAllDepartments: getAllDepartments
+};
