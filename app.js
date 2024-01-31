fd8e76191ed33335785aa5a5f9a3e7dad80d521e
Sheila Hanson <slhanson11@live.com>
2024-01-30 22:52:10 -0600

    added more  functions to, trying to get update section to work properly

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 //Dependencies / packages needed for this application
 const inquirer = require("inquirer");
-//Package to print rows to console
-// const consoleTable = require("console.table");
-//
-// const init =
 const db = require("./utils/server");
 
 function init() {
@@ -34,55 +30,31 @@ function init() {
       // console.log(response);
       //View all departments
       if (response.menu == "View all departments") {
-        //Query database
-        db.query(`SELECT * FROM department`, (err, results) => {
-          // console.log(results);
-          //If err, log it and restart prompt
-          if (err) {
-            console.log(err);
-            // init();
-          }
-          //Display result and restart prompts
-          console.table(results);
-          init();
-        });
+        viewAllDepartments();
       }
       //View all roles
       else if (response.menu == "View all roles") {
-        //Query database
-        db.query(`SELECT * FROM role`, (err, results) => {
-          // console.log(results);
-          //If err, log it and restart prompt
-          if (err) {
-            console.log(err);
-            // init();
-          }
-          //Display result and restart prompts
-          console.table(results);
-          init();
-        });
+        viewAllRoles();
       }
       //View all employees
       else if (response.menu == "View all employees") {
-        //Query database
-        db.query(`SELECT * FROM employee`, (err, results) => {
-          // console.log(results);
-          //If err, log it and restart prompt
-          if (err) {
-            console.log(err);
-            // init();
-          }
-          //Display result and restart prompts
-          console.table(results);
-          init();
-        });
+        viewAllEmployees();
       }
       //Add a department
       else if (response.menu == "Add department") {
-        const addDepartment = [
+        addDepartment();
+      }
+      //Choose to update employee role
+      else if (response.menu == "Update employee role") {
+        updateEmployeeRole();
+      }
+    });
+}
+
+/*const addDepartment = [
           {
             type: "input",
-            name: "departmentName",
+            name: "employee role",
             message: "What is the department name?",
           },
         ];
@@ -113,19 +85,13 @@ function init() {
               });
             }
           );
-        });
-      }
-    });
-}
-
+        });*/
 
 //Choose to add role
-    // repeat
+// repeat
+
 //Choose to add employee
-    // repeat
-//Choose to update employee role
-
-
+// repeat
 
 //Query database
 // let deleteDepartment = "";
@@ -158,5 +124,158 @@ function init() {
 //   }
 //   console.log(result);
 // });
+
+function viewAllDepartments() {
+  //Query database
+  db.query(`SELECT * FROM department`, (err, results) => {
+    // console.log(results);
+    //If err, log it and restart prompt
+    if (err) {
+      console.log(err);
+      // init();
+    }
+    //Display result and restart prompts
+    console.table(results);
+    init();
+  });
+}
+
+function viewAllRoles() {
+  //Query database
+  db.query(`SELECT * FROM role`, (err, results) => {
+    // console.log(results);
+    //If err, log it and restart prompt
+    if (err) {
+      console.log(err);
+      // init();
+    }
+    //Display result and restart prompts
+    console.table(results);
+    init();
+  });
+}
+
+function viewAllEmployees() {
+  //Query database
+  db.query(`SELECT * FROM employee`, (err, results) => {
+    // console.log(results);
+    //If err, log it and restart prompt
+    if (err) {
+      console.log(err);
+      // init();
+    }
+    //Display result and restart prompts
+    console.table(results);
+    init();
+  });
+}
+
+function addDepartment() {
+  const addDepartment = [
+    {
+      type: "input",
+      name: "departmentName",
+      message: "What is the department name?",
+    },
+  ];
+  inquirer.prompt(addDepartment).then((deptNameResponse) => {
+    //Query database
+    //console.log(deptNameResponse.departmentName);
+    db.query(
+      `INSERT INTO department (department_name)
+          VALUES ("${deptNameResponse.departmentName}");`,
+      (err, results) => {
+        //If err, log it and restart prompt
+        if (err) {
+          console.log(err);
+          // init();
+        }
+        db.query(`SELECT * FROM department`, (err, results) => {
+          // console.log(results);
+          //If err, log it and restart prompt
+          if (err) {
+            console.log(err);
+            // init();
+          }
+          //Display result and restart prompts
+          console.table(results);
+          init();
+        });
+      }
+    );
+  });
+}
+
+function updateEmployeeRole() {
+  // Get all employees from the database
+  db.query(`SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee`, (err, results) => {
+    console.log(results);
+    //If err, log it and restart prompt
+    if (err) {
+      console.log(err);
+      // init();
+    }
+    const employeeToUpdate = [
+      {
+        type: "rawlist",
+        name: "employee",
+        message: "Which employee would you like to update?",
+        choices: results,
+      },
+    ];
+    inquirer.prompt(employeeToUpdate).then((response) => {
+      console.log('test 1');
+      console.log(response);
+      console.log('test 2');
+      console.log(response.id);
+      const selectedEmployee = response.employee;
+      console.log('test 3');
+      console.log(selectedEmployee);
+      // Get all roles from the database
+      db.query(`SELECT id, title FROM role`, (err, results) => {
+        // console.log(results);
+        //If err, log it and restart prompt
+        if (err) {
+          console.log(err);
+          // init();
+        }
+        // Prompt the user to pick a role for the employee
+        const employeeRole = [
+          {
+            type: "rawlist",
+            name: "role",
+            message: "Which role would you like to assign to the employee?",
+            choices: results,
+          },
+        ];
+        inquirer.prompt(employeeRole).then((response) => {
+          //If err, log it and restart prompt
+          if (err) {
+            console.log(err);
+            // init();
+          }
+          console.log(response);
+          const selectedRole = response.role;
+          console.log(selectedRole);
+          // Update the employee's role in the database
+          // db.query(
+          //   `UPDATE employee SET role_id = (SELECT id FROM role WHERE title = "${selectedRole}") 
+          //       WHERE id = (SELECT id FROM employee WHERE CONCAT(\`first_name\`, ' ', \`last_name\`) = "${selectedEmployee}")`,
+          //   (err, results) => {
+          //     //If err, log it and restart prompt
+          //     if (err) {
+          //       console.log(err);
+          //       // init();
+          //     }
+          //     console.log(
+          //       selectedEmployee + "'s role has been updated to " + selectedRole
+          //     );
+          //   }
+          // );
+        });
+      });
+    });
+  });
+}
 
 init();
